412fc1209777a5f91aabb2b89390346b0ca1ad98
Jaramillo, Francisco <pacho@live.ca>
2020-03-23 13:47:36 -0400

    make flag menu horizonaltally scrollable. add more countries.

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 import getEmojiFlag from '../../util/getEmojiFlag'
 import styles from './Nav.module.css'
 
-export const countryCodeList = ['CA', 'CO', 'CN', 'US', 'IT', 'IR']
+export const countryCodeList = ['CN', 'CA', 'CO', 'IT', 'IR', 'FR', 'GB', 'JP', 'US']
 
 const CountryLink = ({ countryCode }) => (
-  <Link href='/country/[country]' as={`/country/${countryCode}`}>
+  <Link href="/country/[country]" as={`/country/${countryCode}`}>
     <a className={styles.linkStyle}>{getEmojiFlag(countryCode)}</a>
   </Link>
 )
 
 const WorldLink = () => (
-  <Link href='/'>
+  <Link href="/">
     <a className={styles.linkStyle}>{'🌎'}</a>
   </Link>
 )
@@ -19,7 +19,6 @@ const WorldLink = () => (
 export default () => (
   <div className={styles.navStyle}>
     <WorldLink />
-
     {countryCodeList.map(countryCode => (
       <CountryLink key={countryCode} countryCode={countryCode} />
     ))}
